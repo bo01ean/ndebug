c38768dbea621ccd41f2b96e62458836c4925c4b
Nathan Trujillo <kelvin.beats@gmail.com>
2018-02-22 12:13:40 -0800

    Make it respect depth.

--- debug.js
+++ debug.js
@@ -1,73 +1,73 @@
-
-Object.defineProperty(global, '__stack', {
-  get: function () {
-    var orig = Error.prepareStackTrace;
-    Error.prepareStackTrace = function (_, stack) {
+module.exports = function (space, depth = 2) {
+  Object.defineProperty(global, '__stack', {
+    get: function () {
+      var orig = Error.prepareStackTrace;
+      Error.prepareStackTrace = function (_, stack) {
+        return stack;
+      };
+      var err = new Error;
+      Error.captureStackTrace(err, arguments.callee);
+      var stack = err.stack;
+      Error.prepareStackTrace = orig;
       return stack;
+    }
+  });
+
+  Object.defineProperty(global, '__line', {
+    get: function () {
+      return __stack[depth].getLineNumber();
+    }
+  });
+
+  Object.defineProperty(global, '__function', {
+    get: function () {
+      return __stack[depth].getFunctionName();
+    }
+  });
+
+  Object.defineProperty(global, '__file', {
+    get: function () {
+      return __stack[depth].getFileName();
+    }
+  });
+
+  function censor(censor) {
+    var i = 0;
+    return function (key, value) {
+      if (i !== 0 && typeof censor === 'object' && typeof value == 'object' && censor == value) { return '[Circular]'; }
+      if (i >= 29) { return '[Unknown]'; }
+      ++i;
+      return value;
     };
-    var err = new Error;
-    Error.captureStackTrace(err, arguments.callee);
-    var stack = err.stack;
-    Error.prepareStackTrace = orig;
-    return stack;
   }
-});
 
-Object.defineProperty(global, '__line', {
-  get: function () {
-    return __stack[2].getLineNumber();
+  const regex = /["']?([\.a-z_0-9]+)["']?\s?:(!?.+)(!?\s+)/ig;
+  const groupIndexForKeys = 1;
+
+  function colorify(str, color) {
+    color = (color === undefined) ? 6 : color;
+    var colors = [6, 2, 3, 4, 5, 1];
+    var colorCode = colors[color];
+    return '\u001b[3' + color + ';1m' + str + ' ' + '\u001b[0m';
   }
-});
 
-Object.defineProperty(global, '__function', {
-  get: function () {
-    return __stack[2].getFunctionName();
+  function colorifyObjectKeys(str) {
+    while ((m = regex.exec(str)) !== null) {
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+        m.forEach((match, groupIndex) => {
+            console.log(`Found match, group ${groupIndex}: ${match}`);
+        });
+    }
   }
-});
 
-Object.defineProperty(global, '__file', {
-  get: function () {
-    return __stack[2].getFileName();
+  function parseIt(obj) {
+    var textRepresentation = JSON.stringify(obj, censor(obj));
+    return textRepresentation;
   }
-});
 
-function censor(censor) {
-  var i = 0;
-  return function (key, value) {
-    if (i !== 0 && typeof censor === 'object' && typeof value == 'object' && censor == value) { return '[Circular]'; }
-    if (i >= 29) { return '[Unknown]'; }
-    ++i;
-    return value;
-  };
-}
 
-const regex = /["']?([\.a-z_0-9]+)["']?\s?:(!?.+)(!?\s+)/ig;
-const groupIndexForKeys = 1;
-
-function colorify(str, color) {
-  color = (color === undefined) ? 6 : color;
-  var colors = [6, 2, 3, 4, 5, 1];
-  var colorCode = colors[color];
-  return '\u001b[3' + color + ';1m' + str + ' ' + '\u001b[0m';
-}
-
-function colorifyObjectKeys(str) {
-  while ((m = regex.exec(str)) !== null) {
-      if (m.index === regex.lastIndex) {
-          regex.lastIndex++;
-      }
-      m.forEach((match, groupIndex) => {
-          console.log(`Found match, group ${groupIndex}: ${match}`);
-      });
-  }
-}
-
-function parseIt(obj) {
-  var textRepresentation = JSON.stringify(obj, censor(obj));
-  return textRepresentation;
-}
-
-module.exports = function (space) {
   space = (space === undefined) ? 'default' : space;
   const path = require('path');
   const bug = require('debug')(space);
@@ -78,5 +78,3 @@ module.exports = function (space) {
     return bug([].concat.apply([path.basename(__file) + ':' + __line], arguments).join(' '));
   }
 }
-
-
